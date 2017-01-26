8d6ed318f37665bf9b2b1f8a92c727ebb4ff8b1d
ubu16 <ubu16@ubuDVL-Martinez17>
2017-01-26 20:32:11 +0100

    Versió 0.1 del projecte prjava01MartinezN17

---
+++ src/prjava01MartinezN17/prjava01MartinezN17.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prjava01MartinezN17;
+
+/**
+ *
+ * @author cognom17
+ */
+
+import java.io.*;
+
+public class prjava01MartinezN17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer01MartinezN17.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");bw.newLine();
+            bw.write("  <head>");bw.newLine();
+            bw.write("    <title>");bw.newLine();
+            bw.write("      Nova p&agrave;gina  web de MartinezN17");bw.newLine();
+            bw.write("    </title>");bw.newLine();
+            bw.write("  </head>");bw.newLine();
+            bw.write("  <body>");bw.newLine();
+            bw.write("    Nova p&agrave;gina web de MartinezN17");bw.newLine();
+            bw.write("  </body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
+    }
+}
